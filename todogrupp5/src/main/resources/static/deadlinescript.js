3e259aaca5cba3f3d9bc8a4711ade374ea8fa2ef
samsam1992 <143456029+Samback92@users.noreply.github.com>
2023-12-07 23:45:05 +0100

    gjorde om lite med deadlines

--- todogrupp5/src/main/resources/static/deadlinescript.js
+++ todogrupp5/src/main/resources/static/deadlinescript.js
@@ -1,19 +1,22 @@
 var deadlineElements = document.getElementsByClassName("deadlines");
-
 var currentDate = new Date();
 
 Array.from(deadlineElements).forEach(function (element) {
   var thTextValue = element.innerHTML;
   var deadline = new Date(thTextValue);
+  var timeDiff = Math.abs(deadline.getTime() - currentDate.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  var diffDaysPassed = Math.ceil(timeDiff / (1000 * 3600 * 24)-1);
 
   if (deadline < currentDate) {
-    element.innerHTML = "Deadline har passerat";
+    element.innerHTML = "Deadline: " + deadline.toLocaleDateString() + " och har passerat med " + diffDaysPassed + " dagar";
+    element.classList.add("red-background");
   } else {
-    var timeDiff = Math.abs(deadline.getTime() - currentDate.getTime());
-    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-    element.innerHTML = diffDays + " dagar kvar";
+    element.innerHTML = "Deadline: " + deadline.toLocaleDateString() + " och är " + diffDays + " dagar kvar";
+    element.classList.add("green-background");
   }
 });
+
 
 document.getElementById('taskForm').addEventListener('submit', function(event) {
   var taskDeadline = document.getElementById('taskDeadline').value;
