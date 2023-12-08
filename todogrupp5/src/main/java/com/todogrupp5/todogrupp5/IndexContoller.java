c9acb0586053c434140779f520afdf31f2f216b5
marcusnil94 <marcusnilsson1994@gmail.com>
2023-12-08 08:33:51 +0100

    Fix och lagt till funktion för login/logout

--- todogrupp5/src/main/java/com/todogrupp5/todogrupp5/IndexContoller.java
+++ todogrupp5/src/main/java/com/todogrupp5/todogrupp5/IndexContoller.java
@@ -5,35 +5,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class IndexContoller {
 
     @GetMapping("/")
-    String getHomePage (Model model) {
+    String getHomePage(Model model, HttpSession session) {
         model.addAttribute("users", UserController.users);
+        model.addAttribute("loggedInUser", session.getAttribute("loggedInUser") != null);
         return "index";
     }
 
-    @PostMapping("/")
-    String login(@RequestParam("username") String username, @RequestParam("password") String password) {
-    for (User user : UserController.users) {
-        if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-            user.setLoggedIn(true);
-            return "redirect:/createuser";
-        }
+    @PostMapping("/logout")
+    String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
-    return "index";
-}
-    
-@PostMapping("/logout")
-String logout(@RequestParam(name = "loggedIn", defaultValue = "false") boolean loggedIn) {
-    for (User user : UserController.users) {
-        if (user.isLoggedIn()) {
-            user.setLoggedIn(false);
-            return "redirect:/";
-        }
-    }
-    return "redirect:/";
-}
-}
 
+    @PostMapping("/")
+    String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model, HttpSession session) {
+        boolean userFound = UserController.users.stream()
+                .anyMatch(user -> user.getUsername().equals(username) && user.getPassword().equals(password));
+
+        if (userFound) {
+            session.setAttribute("loggedInUser", true);
+            return "redirect:/";
+        } else {
+            model.addAttribute("loginError", true);
+            return "index";
+        }
+    }
+}
--- todogrupp5/src/main/resources/static/script.js
+++ todogrupp5/src/main/resources/static/script.js
@@ -1,75 +1,174 @@
- if (document.getElementById("loginBtn")) {
-     console.log("loginBtn found");
-    document.getElementById("loginBtn").addEventListener("click", () => {
-         console.log("Button clicked");
-        let username = document.getElementById("username").value;
+let loggedInUser = false;
+let formContainer = document.getElementById("form1");
+let formContainerUser = document.getElementById("form3"); 
+let admin = false;
+let loginBtn = document.getElementById("loginBtn");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
 
-        let password = document.getElementById("password").value;
-        
-        console.log(users);
-        
+updateHeaderText();
+if (formContainerUser) {
+    createUserForm();
+} else {
+    console.log("formContainerUser is null or undefined");
+}
+
+if (formContainer && !localStorage.getItem("loggedInUser")) {
+    createLoginForm();
+} else if (formContainer) {
+    createLoggedInForm();    
+}
+
+function createUserForm(){
+    formContainerUser.innerHTML = ''; 
+    const logoutFormUser = document.createElement('form');
+    logoutFormUser.method = 'post';
+    logoutFormUser.action = '/logout';
+
+    
+    const homeBtn = document.createElement('button');
+    homeBtn.type = 'button'; 
+    homeBtn.id = 'homeBtn';
+    homeBtn.textContent = 'Till Startsidan';
+
+    const logoutBtn = document.createElement('button');
+    logoutBtn.type = 'submit';
+    logoutBtn.id = 'logoutBtn';
+    logoutBtn.textContent = 'Logga ut';
+
+    logoutFormUser.appendChild(homeBtn);
+    logoutFormUser.appendChild(logoutBtn);
+    formContainerUser.appendChild(logoutFormUser);
+
+    logoutFormUser.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        localStorage.removeItem('loggedInUser');
+        console.log('Logout success');
+        window.location.href = '/';
+    });
+
+      homeBtn.addEventListener('click', () => {
+        window.location.href = '/';
+    });
+
+
+}
+
+function createLoginForm() {
+    formContainer.innerHTML = '';
+
+    const loginForm = document.createElement('form');
+    loginForm.method = 'post';
+    loginForm.action = '/'; 
+
+    const usernameInput = document.createElement('input');
+    usernameInput.type = 'text';
+    usernameInput.name = 'username';
+    usernameInput.id = 'username';
+    usernameInput.placeholder = 'Användarnamn';
+    usernameInput.required = true;
+
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.name = 'password';
+    passwordInput.id = 'password';
+    passwordInput.placeholder = 'Lösenord';
+    passwordInput.required = true;
+
+    const loginBtn = document.createElement('button');
+    loginBtn.type = 'submit';
+    loginBtn.id = 'loginBtn';
+    loginBtn.textContent = 'Logga in';
+
+    const newUserBtn = document.createElement('button');
+    newUserBtn.type = 'button'; 
+    newUserBtn.id = 'newUserBtn';
+    newUserBtn.textContent = 'Användare/Skapa ny';
+
+    const loginError = document.createElement('div');
+    loginError.id = 'loginError';
+
+    loginForm.appendChild(usernameInput);
+    loginForm.appendChild(passwordInput);
+    loginForm.appendChild(loginBtn);
+    loginForm.appendChild(newUserBtn);
+    loginForm.appendChild(loginError);
+
+    formContainer.appendChild(loginForm);
+
+    
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        let username = document.getElementById('username').value;
+        let password = document.getElementById('password').value;
+
         let userFound = users.find(user => user.username === username && user.password === password);
 
         if (userFound) {
-            localStorage.setItem("adminLoggedIn", userFound.adminLoggedIn.toString()); 
-            localStorage.setItem("loggedIn", userFound.loggedIn.toString()); 
-        } 
-        
+            loggedInUser = true;
+            localStorage.setItem('loggedInUser', true);
+            console.log('Login Success');
+            createLoggedInForm();
+            updateHeaderText();
+        } else if((username === "admin" && password === "admin")) {
+            admin = true;
+            localStorage.setItem("key1", "admin");
+            localStorage.setItem("key2", "admin");
+            document.dispatchEvent(new Event('loginStatusChanged'));
+            window.location.href = "/adminSida";
+
+        }
         else {
-
-            alert("Invalid username or password");
-         }
-     });
- }
-
-let admin = false;
-loginBtn.addEventListener("click", () => {
-    if (username.value == "admin" && password.value == "admin"){
-        localStorage.setItem("key1",username.value);
-        localStorage.setItem("key2",password.value);
-        admin = true;   
-    } 
-    else{
-        admin = false;
-    }
-
-});
-
-if(localStorage.getItem("key1")=="admin" && localStorage.getItem("key2")== "admin"){
-    admin = true;
-    window.location.href="/adminSida"; 
-}
-else{
-    admin = false;
-}
-
-
-
-if (document.getElementById("logoutBtn")) {
-    document.getElementById("logoutBtn").addEventListener("click", () => {
-        
-        let currentUser = users.find(user => user.loggedIn);
-
-        if (currentUser) {
-            
-            currentUser.loggedIn = false;
-            currentUser.adminLoggedIn = false;
-
-            localStorage.setItem("adminLoggedIn", "false"); 
-            localStorage.setItem("loggedIn", "false"); 
-        } else {
-            console.error("User not found");
+            admin = false;
+            loginError.textContent = 'Felaktigt användarnamn eller lösenord';
         }
     });
-}
 
-if (document.getElementById("newUserBtn")) {
-    document.getElementById("newUserBtn").addEventListener("click", function() {
-        window.location.href = "/createuser";
+    newUserBtn.addEventListener('click', () => {
+        window.location.href = '/createuser';
     });
 }
 
-document.querySelector('form').addEventListener('submit', validatePasswords);
+function createLoggedInForm() {
+    formContainer.innerHTML = '';
+    const logoutForm = document.createElement('form');
+    logoutForm.method = 'post';
+    logoutForm.action = '/logout';
+
+    
+    const logoutBtn = document.createElement('button');
+    logoutBtn.type = 'submit';
+    logoutBtn.id = 'logoutBtn';
+    logoutBtn.textContent = 'Logga ut';
+
+    const newUserBtn = document.createElement('button');
+    newUserBtn.type = 'button'; 
+    newUserBtn.id = 'newUserBtn';
+    newUserBtn.textContent = 'Användare/Skapa ny';
+
+    logoutForm.appendChild(logoutBtn);
+    logoutForm.appendChild(newUserBtn);
+    
+    formContainer.appendChild(logoutForm);
+
+    logoutForm.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+        localStorage.removeItem('loggedInUser');
+        console.log('Logout success');
+        createLoginForm();
+        updateHeaderText();
+    });
+
+    newUserBtn.addEventListener('click', () => {
+        window.location.href = '/createuser';
+    });
+  
+}
+
+let form2 = document.querySelector('#form2');
+if (form2) {
+    form2.addEventListener('submit', validatePasswords);
+}
 
 function validatePasswords(event) {
     const password = getPassword();
@@ -99,4 +198,12 @@ function displayPasswordError() {
 
 function preventFormSubmission(event) {
     event.preventDefault();
+}
+
+function updateHeaderText() {
+    if (document.getElementById('todoheader') && localStorage.getItem('loggedInUser')) {
+        document.getElementById('todoheader').textContent = 'Du är inloggad på ToDo-List';
+    } else if (document.getElementById('todoheader') && !localStorage.getItem('loggedInUser')){
+        document.getElementById('todoheader').textContent = 'ToDo-List login';
+    }
 }
